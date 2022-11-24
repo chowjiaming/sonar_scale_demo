aa7ae7d3e7ea0b2fda2bcda849ff5a7c67d9f649
Joseph Chow <joseph.chow@qii.ai>
2022-11-24 17:25:35 -0500

    restyled setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
+import demoStitch from "./assets/demo-stitch.png";
 import "./App.css";
 import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
-      <StyledHeader>Sonar Stitch Scalebar Demo</StyledHeader>
+      <AppContainer>
+        <StyledHeader>Sonar Stitch Scalebar Demo</StyledHeader>
+        <img src={demoStitch} alt="Stitched sonar demo" />
+      </AppContainer>
     </div>
   );
 }
 
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 const StyledHeader = styled.h1`
+  font-size: 3.2em;
+  line-height: 1.1;
   color: #213547;
 `;
 
