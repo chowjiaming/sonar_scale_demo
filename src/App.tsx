82b382323fb21131fd349bba647a7db1f96dfcb7
Joefs <joefstack@gmail.com>
2022-11-25 02:02:34 -0500

    setup stitch resize

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import {useState} from 'react';
 import {ControlPanel} from './Components/ControlPanel';
 import {SonarStitchImage} from './Components/SonarStitchImage';
 import styled from 'styled-components';
 
 function App() {
+  const [width, setWidth] = useState<number>(800);
   return (
     <div className="App">
       <AppContainer>
         <StyledHeader>Sonar Stitch Scalebar Demo</StyledHeader>
         <ControlPanel />
-        <SonarStitchImage />
+        <SonarStitchImage width={width} setWidth={setWidth} />
       </AppContainer>
     </div>
   );
--- src/Components/SonarStitchImage.tsx
+++ src/Components/SonarStitchImage.tsx
@@ -1,8 +1,72 @@
-import React from 'react';
+import {useState} from 'react';
 import demoStitch from '../assets/demo-stitch.png';
+import styled from 'styled-components';
 
-export function SonarStitchImage(): JSX.Element {
-  return <img src={demoStitch} alt="Stitched sonar demo" />;
+type Props = {
+  width: number;
+  setWidth: React.Dispatch<React.SetStateAction<number>>;
+};
+export function SonarStitchImage(props: Props): JSX.Element {
+  const handleMouseRight = (
+    mouseDownEvent: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    const startWidth = props.width;
+    const startPosition = mouseDownEvent.pageX;
+    function onMouseMove(mouseMoveEvent: MouseEvent) {
+      props.setWidth(() => startWidth - startPosition + mouseMoveEvent.pageX);
+    }
+    function onMouseUp() {
+      document.body.removeEventListener('mousemove', onMouseMove);
+    }
+    document.body.addEventListener('mousemove', onMouseMove);
+    document.body.addEventListener('mouseup', onMouseUp, {once: true});
+  };
+  const handleMouseLeft = (
+    mouseDownEvent: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    const startWidth = props.width;
+    const startPosition = mouseDownEvent.pageX;
+    function onMouseMove(mouseMoveEvent: MouseEvent) {
+      props.setWidth(() => startWidth + startPosition - mouseMoveEvent.pageX);
+    }
+    function onMouseUp() {
+      document.body.removeEventListener('mousemove', onMouseMove);
+    }
+    document.body.addEventListener('mousemove', onMouseMove);
+    document.body.addEventListener('mouseup', onMouseUp, {once: true});
+  };
+  return (
+    <ImageContainer width={props.width}>
+      <LeftResizeButton onMouseDown={handleMouseLeft} />
+      <StyledSonarStitchImage src={demoStitch} alt="Stitched sonar demo" />
+      <RightResizeButton onMouseDown={handleMouseRight} />
+    </ImageContainer>
+  );
 }
+
+const ImageContainer = styled.div<{width: number}>`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: ${(props) => props.width}px;
+`;
+const StyledSonarStitchImage = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+const ResizeButton = styled.button`
+  position: absolute;
+  height: 100%;
+  cursor: ew-resize;
+  background-color: transparent;
+  border: none;
+`;
+const RightResizeButton = styled(ResizeButton)`
+  right: 0;
+`;
+const LeftResizeButton = styled(ResizeButton)`
+  left: 0;
+`;
 
 SonarStitchImage.displayName = 'SonarStitchImage';
